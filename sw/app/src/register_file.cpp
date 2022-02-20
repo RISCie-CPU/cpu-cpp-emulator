0778bae7bfd75532af95e0228cf4bbb798f64eaf
wykys <vvykys@gmail.com>
2022-02-20 16:02:06 +0100

    Updated register file test format

--- sw/app/src/register_file.cpp
+++ sw/app/src/register_file.cpp
@@ -9,9 +9,11 @@ RegisterFile::RegisterFile()
 
 void RegisterFile::Test()
 {
+    using namespace std;
     for (int i = 0; i < 32; i++)
         info(
-            "R" << std::dec << std::setfill('0') << std::setw(2) << i << std::hex << std::uppercase << ": 0x" << std::setfill('0') << std::setw(8) << register_array[i].stored_value
+            "R" << dec << setfill('0') << setw(2) << i
+                << hex << uppercase << ": 0x" << setfill(' ') << setw(8) << register_array[i].stored_value
         );
 }
 
